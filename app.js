1fe96fab41da2ecffe1d2ba86556a5619c246a2d
Sahil <garg07825@gmail.com>
2023-07-26 09:00:32 +0530

    Added routes folder and mader article file in routes

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 import express from "express";
-
+import articleRouter from "./routes/articles.js";
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set("/view", "view")
 
-app.get("/", (req, res) => {
-    res.render('index');
-})
+app.use("", articleRouter);
 
 app.listen(5000, () => {
     console.log("Server is wrking on port no 5000");
